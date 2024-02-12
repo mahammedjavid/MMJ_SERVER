c45098c751ad58569dd7f2e6c3f9d4e48c79a954
Mahammed Javid <mahammedjavid8@gmail.com>
2024-02-12 22:32:18 +0530

    SKU generate issue is fixed

--- src/utils/generateSKU.ts
+++ src/utils/generateSKU.ts
@@ -1,4 +1,5 @@
 function generateNextSequentialID(lastValue: any) {
+  console.log("00000000000000 - last value is ----------------",lastValue)
   const prefix = "MMJ";
   const counter = lastValue ? parseInt(lastValue.substring(3)) + 1 : 0;
   const formattedCounter = counter.toString().padStart(5, "0");
