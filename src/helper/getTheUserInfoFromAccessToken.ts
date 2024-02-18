3e4b841eb78cdfb60c17ded4fecd813c85facab3
Mahammed Javid <mahammedjavid8@gmail.com>
2024-02-18 14:25:00 +0530

    wishlist and cart flag is added in product get if token exist

--- src/helper/getTheUserInfoFromAccessToken.ts
+++ src/helper/getTheUserInfoFromAccessToken.ts
@@ -1,14 +1,14 @@
 import { Request } from 'express';
 import * as jwt from 'jsonwebtoken';
 
-function getTheUserInfoFromJwt(req: Request):any {
+function getTheUserInfoFromJwt(req: Request , required:any):any {
   try {
     if (req?.headers?.authorization) {
       const accessToken = req?.headers?.authorization //?.split(' ')[1]; // for passport add Bearer
       const decodedToken = jwt.decode(accessToken as string);
       return decodedToken;
     }
-    throw new Error('Access token not found');
+    if(required !='optional')throw new Error('Access token not found');
   } catch (error) {
     throw error;
   }
